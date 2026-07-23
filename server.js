14259b49993706b9f3ca3b134f11e9b392db6a5a
Kevin Jason Jerald Dcosta <kevinjasondcosta@gmail.com>
2026-07-23 23:57:43 +0530

    Completed Day 5 Express GET and POST routes

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-// Serve all static files (HTML, CSS, JS)
+// Middleware to read form data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve HTML files
 app.use(express.static(__dirname));
 
-// Route for home page
+// ✅ GET route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.send("Welcome to Blog Home Page");
 });
 
+// ✅ POST route
+app.post('/add-blog', (req, res) => {
+    const { title, content } = req.body;
+
+    res.send(`Blog received: ${title} - ${content}`);
+});
+
+// Start server
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+    console.log("Server running on http://localhost:3000");
 });
