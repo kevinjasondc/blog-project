23c06a1463fe37f7fd47de9b8c9438b55cd3d144
Kevin Jason Jerald Dcosta <kevinjasondcosta@gmail.com>
2026-07-24 00:41:30 +0530

    Completed Day 7 View Blogs

--- server.js
+++ server.js
@@ -5,25 +5,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// JavaScript array to store blogs
+// Store blog posts
 let blogs = [];
 
-// GET route
+// Home page
 app.get('/', (req, res) => {
-    res.send("Welcome to Blog Home Page");
+    res.sendFile(__dirname + "/index.html");
 });
 
-// POST API to add a blog
+// GET API - View all blogs
+app.get('/blogs', (req, res) => {
+    res.json(blogs);
+});
+
+// POST API - Add a blog
 app.post('/add-blog', (req, res) => {
 
     const { title, content } = req.body;
 
-    const newBlog = {
+    blogs.push({
         title,
         content
-    };
-
-    blogs.push(newBlog);
+    });
 
     res.send("Blog added successfully!");
 
